68cdc2b1e429ab8dfc5a6a46a1ca46c7c6e48962
Unknown <tsiclovan947@west-mec.org>
2018-08-29 18:27:58 -0700

    XHR Objects
    Instantiate an XHR object by creating a function with a try-catch statement to catch errors; Tested for an XHR object; Protected against an open request

--- script.js
+++ script.js
@@ -12,29 +12,53 @@
 // global variables
 var selectedCity = "Tucson, AZ"; //default location
 var weatherReport = null;
+//var to hold our XHR Object
+var httpRequest = false;
+
+//function to get a request object
+function getRequestObject() {
+    //Instantiate an XHR object
+    try {
+        httpRequest = new XMLHttpRequest();
+    }
+    catch (errorMessage) {
+        document.querySelector("p.error").innerHTML = "Forecast not supported by your browser.";
+        document.querySelector("p.error").style.display = "block";
+        return false;
+}
+    return httpRequest;
+}
 
 // Get the weather in response to click events on city locations
 // and for default city on page load
 function getWeather(evt) {
-   var latitude;
-   var longitude;
-   if (evt.type !== "load") {
-      if (evt.target) {
-         selectedCity = evt.target.innerHTML;
-      } else if (evt.srcElement) {
-         selectedCity = evt.srcElement.innerHTML;
-      }
-   }
-   if (selectedCity === "Tucson, AZ") {
-      latitude = 37.7577;
-      longitude = -122.4376;
-   } else if (selectedCity === "Chicago, IL") {
-      latitude = 41.8337329;
-      longitude = -87.7321555;
-   } else if (selectedCity === "Montreal, QC") {
-      latitude = 45.5601062;
-      longitude = -73.7120832;
-   }
+    var latitude;
+    var longitude;
+    if (evt.type !== "load") {
+        if (evt.target) {
+            selectedCity = evt.target.innerHTML;
+        } else if (evt.srcElement) {
+            selectedCity = evt.srcElement.innerHTML;
+        }
+    }
+    if (selectedCity === "Tucson, AZ") {
+        latitude = 37.7577;
+        longitude = -122.4376;
+    } else if (selectedCity === "Chicago, IL") {
+        latitude = 41.8337329;
+        longitude = -87.7321555;
+    } else if (selectedCity === "Montreal, QC") {
+        latitude = 45.5601062;
+        longitude = -73.7120832;
+    }
+    //Test for XHR object
+    if (!httpRequest) {
+        httpRequest = getRequestObject();
+    }
+    //Protect against an open request
+    httpRequest.abort();
+    httpRequest.open("get", "solar.php?" + "lat=" + latitude + "&lng=" + longitude, true);
+    httpRequest.send(null);
 }
 
 // Retrieve li elements holding city location choices
@@ -42,17 +66,17 @@ var locations = document.querySelectorAll("section ul li");
 // add click event listeners to all city location elements
 // Event handler will be getWeather()
 for (var i = 0; i < locations.length; i++) {
-   if (locations[i].addEventListener) {
-      locations[i].addEventListener("click", getWeather, false);
-   } else if (locations[i].attachEvent) {
-      locations[i].attachEvent("onclick", getWeather);
-   }
+    if (locations[i].addEventListener) {
+        locations[i].addEventListener("click", getWeather, false);
+    } else if (locations[i].attachEvent) {
+        locations[i].attachEvent("onclick", getWeather);
+    }
 }
 
 // Add load event listeners to get weather for default locations
 // default location event hander is getWeather() 
 if (window.addEventListener) {
-   window.addEventListener("load", getWeather, false);
+    window.addEventListener("load", getWeather, false);
 } else if (window.attachEvent) {
-   window.attachEvent("onload", getWeather);
+    window.attachEvent("onload", getWeather);
 }
